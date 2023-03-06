6e6316c6f0e2a36a98e0e58765200e8cd5d8709d
Fatoumata Kaba <fkabanoun@gmail.com>
2023-03-06 15:16:30 -0500

    Server.js can get restaurants

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
+const seedRestaurant = require("./seedData");
 
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
 
+app.get('/restaurants', async(request, response)=> {
+    const allRestaurants = await Restaurant.findAll();
+    await response.json(allRestaurants);
+});
 
 app.listen(port, () => {
     sequelize.sync();
     console.log("Your server is listening on port " + port);
+    console.log(' http://localhost:3000/restaurants')
 })
